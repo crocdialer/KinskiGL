94d245d66cc51317b87b7e6ef480d37732f9fa0f
Fabian <crocdialer@artcom.de>
2012-05-27 11:31:10 +0200

    progress on simple demo

--- src/kinskiGL/App.h
+++ src/kinskiGL/App.h
@@ -5,27 +5,46 @@
 
 namespace kinski{
     
-    class App
+    class App : public boost::enable_shared_from_this<App>
     {
     public:
+        typedef boost::shared_ptr<App> Ptr;
+        typedef boost::weak_ptr<App> WeakPtr;
+        
+        static Ptr getInstance();
+        
         App(const int width = 640, const int height = 480);
         virtual ~App();
         
-        static void resize(int w, int h);
+        void resize(const int w, const int h);
         int run();
         
-        // you are supposed to implement these
+        // you are supposed to implement these in a subclass
         virtual void setup() = 0;
         virtual void draw() = 0;
-        virtual void update(const float delta) = 0;
+        virtual void update(const float timeDelta) = 0;
+        
+        inline float getWidth(){return m_width;};
+        inline float getHeight(){return m_height;};
+        
+        double getApplicationTime();
         
         void setTweakBarVisible(bool b){m_displayTweakBar = b;};
         bool isTweaBarVisible(){return m_displayTweakBar;};
         TwBar* getTweakBar(){return m_tweakBar;};
         
     private:
-
+        
+        static WeakPtr s_instance;
+        
+        // GLFW static callbacks
+        static void s_resize(int w, int h){getInstance()->resize(w, h);};
+        
+        // internal initialization. performed when run is invoked
+        void init();
+        
         GLint m_running;
+        double m_lastTimeStamp;
         
         int m_width, m_height;
         bool m_fullscreen;
@@ -33,6 +52,7 @@ namespace kinski{
         TwBar *m_tweakBar;
         bool m_displayTweakBar;
         float m_testFloat;
+        
     };    
 }
 #endif // KINSKI_APP_H
--- src/samples/simple.cpp
+++ src/samples/simple.cpp
@@ -1,6 +1,7 @@
 #include "kinskiGL/App.h"
 #include "kinskiGL/Texture.h"
 #include "kinskiGL/Shader.h"
+#include "Data.h"
 
 using namespace std;
 using namespace kinski;
@@ -13,27 +14,96 @@ private:
     gl::Texture m_texture;
     gl::Shader m_shader;
     
+    GLuint m_vertexArrayObject;
+    GLuint m_vertexBuffer;
+    
 public:
     
     void setup()
     {
-        m_projectionMatrix = glm::ortho(0, 1, 0, 1, 0, 1);
+        glEnable(GL_TEXTURE_2D);
+        
+        try 
+        {
+            m_shader.loadFromFile("texShader.vsh", "texShader.fsh");
+        }
+        catch (std::exception &e) 
+        {
+            fprintf(stdout, "%s\n",e.what());
+            
+            
+        }
+        
+        // orthographic projection with a [0,1] coordinate space
+        //m_projectionMatrix = glm::ortho(0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f);
+        
+        // create and bind vertex array object
+        glGenVertexArrays(1, &m_vertexArrayObject);
+        glBindVertexArray(m_vertexArrayObject);
+        
+        // create vertex buffers and attrib locations
+        glGenBuffers(1, &m_vertexBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_CubeVertexData), g_CubeVertexData, GL_STATIC_DRAW);
+        
+        GLuint positionAttribLocation = m_shader.getAttribLocation("a_position");
+        GLuint normalAttribLocation = m_shader.getAttribLocation("a_normal");
+        GLuint texCoordAttribLocation = m_shader.getAttribLocation("a_texCoord");
+        
+        // define attrib pointers
+        GLsizei stride = 8 * sizeof(GLfloat);
+        
+        glEnableVertexAttribArray(positionAttribLocation);
+        glVertexAttribPointer(positionAttribLocation, 3, GL_FLOAT, GL_FALSE,
+                              stride,
+                              BUFFER_OFFSET(0));
+        glEnableVertexAttribArray(normalAttribLocation);
+        glVertexAttribPointer(normalAttribLocation, 3, GL_FLOAT, GL_FALSE,
+                              stride,
+                              BUFFER_OFFSET(3 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(texCoordAttribLocation);
+        glVertexAttribPointer(texCoordAttribLocation, 2, GL_FLOAT, GL_FALSE,
+                              stride,
+                              BUFFER_OFFSET(6 * sizeof(GLfloat)));
+        // unbind buffers
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
     }
     
-    void update(const float delta)
+    void update(const float timeDelta)
     {
-        
+        float aspect = fabsf(getWidth() / getHeight());
+        m_projectionMatrix = glm::perspective(65.0f, aspect, 0.1f, 100.0f);
+
+        glm::mat4 modelViewMatrix;
+        m_shader.uniform("u_modelViewProjectionMatrix", 
+                         m_projectionMatrix * modelViewMatrix);
     }
     
     void draw()
     {
-        // OpenGL rendering goes here...
+        // Texture and Shader bound for this scope
+        gl::scoped_bind<gl::Texture> texBind(m_texture);
+        gl::scoped_bind<gl::Shader> shaderBind(m_shader);
+        
+        if(m_texture)
+        {
+            m_shader.uniform("u_textureMap", m_texture.getBoundTextureUnit());
+            m_shader.uniform("u_textureMatrix", m_texture.getTextureMatrix());
+        }
+        
+        glBindVertexArray(m_vertexArrayObject);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
     }
 };
 
 int main(int argc, char *argv[])
 {
-    PoopApp theApp;
+    char b[512];
+    getcwd(b, sizeof(b));
+    printf("working dir: '%s'\n", b);
     
-    return theApp.run();
+    PoopApp::Ptr theApp(new PoopApp);
+    
+    return theApp->run();
 }
