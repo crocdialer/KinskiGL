fdc679489a341102f0ec62b00a310fb34f9b6a6d
crocdialer <crocdialer@googlemail.com>
2013-11-14 12:18:10 +0100

    still push some raspi cosmetics

--- src/kinskiGL/Object3D.cpp
+++ src/kinskiGL/Object3D.cpp
@@ -7,6 +7,7 @@
 //  http://www.boost.org/LICENSE_1_0.txt)
 // __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
 
+#include <algorithm>
 #include "kinskiGL/geometry_types.h"
 #include "Object3D.h"
 
