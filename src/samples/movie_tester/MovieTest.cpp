f9d6e9d4deefb9b6e2a6159186c372bc7adf355c
crocdialer <crocdialer@googlemail.com>
2014-02-05 17:01:39 +0100

    added avfoundation based movie sample

---
+++ src/samples/movie_tester/MovieTest.cpp
@@ -0,0 +1,99 @@
+//
+//  CoreVideoTest.cpp
+//  kinskiGL
+//
+//  Created by Fabian on 29/01/14.
+//
+//
+
+#include "MovieTest.h"
+
+using namespace std;
+using namespace kinski;
+using namespace glm;
+
+
+/////////////////////////////////////////////////////////////////
+
+void CoreVideoTest::setup()
+{
+    ViewerApp::setup();
+    
+    m_font.load("Courier New Bold.ttf", 18);
+    outstream_gl().set_color(gl::COLOR_WHITE);
+    outstream_gl().set_font(m_font);
+    
+    registerProperty(m_movie_path);
+    observeProperties();
+    create_tweakbar_from_component(shared_from_this());
+    load_settings();
+}
+
+/////////////////////////////////////////////////////////////////
+
+void CoreVideoTest::update(float timeDelta)
+{
+    m_movie.copy_frame_to_texture(m_textures[0]);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void CoreVideoTest::draw()
+{
+    if(m_textures[0])
+        gl::drawTexture(m_textures[0], gl::windowDimension());
+    
+    gl::drawText2D(kinski::as_string(m_movie.current_time()), m_font);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void CoreVideoTest::keyPress(const KeyEvent &e)
+{
+    ViewerApp::keyPress(e);
+    
+    switch (e.getCode())
+    {
+        case GLFW_KEY_P:
+            m_movie.pause();
+            break;
+            
+        case GLFW_KEY_LEFT:
+            m_movie.seek_to_time(m_movie.current_time() - 5);
+            break;
+        
+        case GLFW_KEY_RIGHT:
+            m_movie.seek_to_time(m_movie.current_time() + 5);
+            break;
+            
+        default:
+            break;
+    }
+}
+
+/////////////////////////////////////////////////////////////////
+
+void CoreVideoTest::got_message(const std::string &the_message)
+{
+    LOG_INFO<<the_message;
+}
+
+/////////////////////////////////////////////////////////////////
+
+void CoreVideoTest::tearDown()
+{
+    LOG_PRINT<<"ciao movie sample";
+}
+
+/////////////////////////////////////////////////////////////////
+
+void CoreVideoTest::updateProperty(const Property::ConstPtr &theProperty)
+{
+    ViewerApp::updateProperty(theProperty);
+    
+    if(theProperty == m_movie_path)
+    {
+        m_movie.load(*m_movie_path);
+        m_movie.play();
+    }
+}
---
+++ src/samples/movie_tester/MovieTest.h
@@ -0,0 +1,44 @@
+//
+//  CoreVideoTest.h
+//  kinskiGL
+//
+//  Created by Fabian on 29/01/14.
+//
+//
+
+#ifndef __kinskiGL__CoreVideoTest__
+#define __kinskiGL__CoreVideoTest__
+
+#include "kinskiApp/ViewerApp.h"
+
+#include "kinskiGL/Texture.h"
+#include "MovieController.h"
+
+namespace kinski
+{
+    class CoreVideoTest : public ViewerApp
+    {
+    private:
+        
+        gl::Font m_font;
+        MovieController m_movie;
+        vector<gl::Texture> m_textures{4};
+        
+        // properties
+        Property_<string>::Ptr m_movie_path = Property_<string>::create("movie path", "");
+        
+    public:
+        
+        void setup();
+        void update(float timeDelta);
+        void draw();
+        void got_message(const std::string &the_message);
+        void tearDown();
+        void updateProperty(const Property::ConstPtr &theProperty);
+        
+        void keyPress(const KeyEvent &e);
+        
+    };
+}// namespace kinski
+
+#endif /* defined(__kinskiGL__CoreVideoTest__) */
