268406855879ffcf269e707e4dac221cf7155646
Croc Dialer <crocdialer@tambour.local>
2014-09-27 15:27:59 +0200

    raspi cosmetics

--- src/core/Exception.h
+++ src/core/Exception.h
@@ -18,7 +18,7 @@ namespace kinski
     {
     public:
         explicit Exception(const std::string &msg): std::runtime_error(msg){};
-        virtual ~Exception() throw(){};
+        virtual ~Exception(){};
     };
     
 } // namespace kinski
