be0069058f1e43f3ed91a46b8717f0238a2f0b62
crocdialer <crocdialer@googlemail.com>
2015-11-05 18:43:41 +0100

    replaced utf8 conversion code with libc++ stuff

--- src/gl/Font.cpp
+++ src/gl/Font.cpp
@@ -13,6 +13,8 @@
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb_truetype.h"
 
+#include <codecvt>
+
 namespace kinski { namespace gl {
     
     struct string_mesh_container
@@ -48,39 +50,6 @@ namespace kinski { namespace gl {
                 dst_row_start[c * bytes_per_pixel] = src_row_start[c * bytes_per_pixel];
             }
         }
-    }
-    
-    // Copyright (c) 2008-2009 Bjoern Hoehrmann <bjoern@hoehrmann.de>
-    // See http://bjoern.hoehrmann.de/utf-8/decoder/dfa/ for details.
-    
-#define UTF8_ACCEPT 0
-#define UTF8_REJECT 1
-    
-    uint32_t inline decode(uint32_t* state, uint32_t* codep, uint32_t byte)
-    {
-        static const uint8_t utf8d[] = {
-            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 00..1f
-            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 20..3f
-            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 40..5f
-            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 60..7f
-            1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9, // 80..9f
-            7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7, // a0..bf
-            8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, // c0..df
-            0xa,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x4,0x3,0x3, // e0..ef
-            0xb,0x6,0x6,0x6,0x5,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8, // f0..ff
-            0x0,0x1,0x2,0x3,0x5,0x8,0x7,0x1,0x1,0x1,0x4,0x6,0x1,0x1,0x1,0x1, // s0..s0
-            1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,1, // s1..s2
-            1,2,1,1,1,1,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1, // s3..s4
-            1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1, // s5..s6
-            1,3,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1, // s7..s8
-        };
-        
-        uint32_t type = utf8d[byte];
-        *codep = (*state != UTF8_ACCEPT) ?
-        (byte & 0x3fu) | (*codep << 6) :
-        (0xff >> type) & (byte);
-        *state = utf8d[256 + *state*16 + type];
-        return *state;
     }
     
     struct Font::Obj
@@ -175,23 +144,24 @@ namespace kinski { namespace gl {
         typedef std::list< std::pair<Area<uint32_t>, Area<uint32_t> > > Area_Pairs;
         Area_Pairs area_pairs;
         
-        std::string::const_iterator it = theText.begin();
-        for (; it != theText.end(); ++it)
+        // the UTF-8 / UTF-16 standard conversion facet
+        std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> utf16conv;
+        std::u16string utf16 = utf16conv.from_bytes(theText);
+        
+        for (auto it = utf16.begin(); it != utf16.end(); ++it)
         {
-            uint32_t codepoint = 0;
-            uint32_t state = UTF8_ACCEPT;
-            
-            while (decode(&state, &codepoint, (uint8_t)*it)){ ++it; }
+            const auto &codepoint = *it;
             
             //new line
-            if(codepoint == 10)
+            if(codepoint == '\n')
             {
                 x = 0;
                 y += m_obj->line_height;
+                continue;
             }
             
             stbtt_GetBakedQuad(m_obj->char_data, m_obj->bitmap_width, m_obj->bitmap_height,
-                               codepoint-32, &x, &y, &q, 1);
+                               codepoint - 32, &x, &y, &q, 1);
             
             int w = q.x1 - q.x0;
             int h = q.y1 - q.y0;
@@ -270,20 +240,21 @@ namespace kinski { namespace gl {
         uint32_t max_y = 0;
         stbtt_aligned_quad q;
         std::list<stbtt_aligned_quad> quads;
-   
-        std::string::const_iterator it = theText.begin();
-        for (; it != theText.end(); ++it)
+        
+        // the UTF-8 / UTF-16 standard conversion facet
+        std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> utf16conv;
+        std::u16string utf16 = utf16conv.from_bytes(theText);
+        
+        for (auto it = utf16.begin(); it != utf16.end(); ++it)
         {
-            uint32_t codepoint = 0;
-            uint32_t state = UTF8_ACCEPT;
-            
-            while (it != theText.end() && decode(&state, &codepoint, (uint8_t)*it)){ ++it; }
+            const auto &codepoint = *it;
             
             //new line
-            if(codepoint == 10)
+            if(codepoint == '\n')
             {
                 x = 0;
                 y += m_obj->line_height;
+                continue;
             }
             stbtt_GetBakedQuad(m_obj->char_data, m_obj->bitmap_width, m_obj->bitmap_height,
                                codepoint - 32, &x, &y, &q, 1);
