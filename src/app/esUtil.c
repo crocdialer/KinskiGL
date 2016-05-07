8f939d51fd580eb5a5fb870a846e87e62d694586
crocdialer <crocdialer@googlemail.com>
2016-05-07 18:48:15 +0200

    blank background and font search path for raspi app

--- src/app/esUtil.c
+++ src/app/esUtil.c
@@ -159,7 +159,7 @@ EGLBoolean WinCreate(ESContext *esContext, const char *title)
         vc_dispmanx_rect_set(&img_rect, 0, 0, 1, 1);
         vc_dispmanx_rect_set(&poop_rect, 0, 0, 0, 0);
         resource = vc_dispmanx_resource_create(type, 1 /*width*/, 1 /*height*/, &vc_image_ptr);
-        vc_dispmanx_resource_write_data(resource, type, sizeof(image), &image, &poop_rect);
+        vc_dispmanx_resource_write_data(resource, type, sizeof(image), &image, &img_rect);
         vc_dispmanx_element_add(dispman_update, dispman_display, bg_layer, &poop_rect, resource, &src_rect,
                                 DISPMANX_PROTECTION_NONE, NULL, NULL,
                                 DISPMANX_STEREOSCOPIC_MONO);
@@ -169,7 +169,7 @@ EGLBoolean WinCreate(ESContext *esContext, const char *title)
     dispman_element = vc_dispmanx_element_add (dispman_update, dispman_display,
                                                egl_layer, &dst_rect, 0/*src*/,
                                                &src_rect, DISPMANX_PROTECTION_NONE,
-                                               0 /*alpha*/, 0/*clamp*/, 0/*transform*/);
+                                               NULL /*alpha*/, NULL/*clamp*/, NULL/*transform*/);
     nativewindow.element = dispman_element;
     nativewindow.width = display_width;
     nativewindow.height = display_height;
