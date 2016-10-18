812826204602c80ddab8a229cdf9d5b50bfdd4c6
crocdialer <crocdialer@googlemail.com>
2016-10-18 19:58:18 +0200

    pause behaviour in media module

--- modules/media/rpi/MediaController.cpp
+++ modules/media/rpi/MediaController.cpp
@@ -534,7 +534,6 @@ namespace kinski{ namespace media
             set_rate(m_impl->m_rate);
 
             // start thread
-            m_impl->m_playing = true;
             m_impl->m_thread = std::thread(std::bind(&MediaControllerImpl::thread_func, m_impl.get()));
         }
     }
