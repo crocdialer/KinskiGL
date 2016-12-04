4573657ea0c8e726665515cbf7c0e31685c9874c
crocdialer <crocdialer@googlemail.com>
2016-12-04 15:08:51 +0100

    minor change in Logger, faster comile with explicit template instantions for Property_ class

--- src/core/Property.cpp
+++ src/core/Property.cpp
@@ -10,10 +10,43 @@
 
 namespace kinski
 {
-    template class Property_<int>;
+    template class Property_<int32_t>;
     template class Property_<uint32_t>;
+    template class Property_<int64_t>;
+    template class Property_<uint64_t>;
+    template class Property_<size_t>;
     template class Property_<float>;
     template class Property_<double>;
     template class Property_<bool>;
-//    template class Property_<std::string>;
+    
+    template<> Property_<std::string>& Property_<std::string>::operator-=(const std::string &theVal)
+    { return *this; };
+    
+    template<> Property_<std::string>& Property_<std::string>::operator*=(const std::string &theVal)
+    { return *this; };
+    
+    template<> Property_<std::string>& Property_<std::string>::operator/=(const std::string &theVal)
+    { return *this; };
+    
+    template class Property_<std::string>;
+    
+    template<>
+    Property_<std::vector<std::string>>&
+    Property_<std::vector<std::string>>::operator+=(const std::vector<std::string> &theVal)
+    { return *this; };
+    
+    template<>
+    Property_<std::vector<std::string>>&
+    Property_<std::vector<std::string>>::operator-=(const std::vector<std::string> &theVal)
+    { return *this; };
+    
+    template<> Property_<std::vector<std::string>>&
+    Property_<std::vector<std::string>>::operator*=(const std::vector<std::string> &theVal)
+    { return *this; };
+    
+    template<> Property_<std::vector<std::string>>&
+    Property_<std::vector<std::string>>::operator/=(const std::vector<std::string> &theVal)
+    { return *this; };
+    
+    template class Property_<std::vector<std::string>>;
 }
