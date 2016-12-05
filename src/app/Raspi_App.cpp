025328e57178a8e7875443ef6c64ce60f74f156b
crocdialer <crocdialer@googlemail.com>
2016-12-05 17:43:25 +0100

    added input periodic scan for input-devices on rpi

--- src/app/Raspi_App.cpp
+++ src/app/Raspi_App.cpp
@@ -86,12 +86,13 @@ namespace kinski
 
             main_queue().submit([this, mouse_fd, keyboard_fd]
             {
-                if(mouse_fd && !m_mouse_fd){ LOG_TRACE << "mouse connected"; }
+                if(mouse_fd && !m_mouse_fd){ LOG_TRACE << "mouse connected"; m_mouse_fd = mouse_fd}
                 else if(!mouse_fd && m_mouse_fd){ LOG_TRACE << "mouse disconnected"; }
-                if(keyboard_fd && !m_keyboard_fd){ LOG_TRACE << "keyboard connected"; }
+                if(keyboard_fd && !m_keyboard_fd){ LOG_TRACE << "keyboard connected"; m_keyboard_fd = keyboard_fd}
                 else if(!keyboard_fd && m_keyboard_fd){ LOG_TRACE << "keyboard disconnected"; }
-                m_mouse_fd = mouse_fd;
-                m_keyboard_fd = keyboard_fd;
+
+                // if(m_mouse_fd){ close(m_mouse_fd); m_mouse_fd = mouse_fd; }
+                // if(m_keyboard_fd){ close(m_keyboard_fd); m_keyboard_fd = keyboard_fd; }
             });
         });
         m_timer_device_scan.set_periodic();
