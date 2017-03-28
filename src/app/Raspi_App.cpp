3495d6f2adfe928a64b6f7e40f51b6f18bc3f6d0
crocdialer <crocdialer@googlemail.com>
2017-03-28 19:55:01 +0200

    raspi fixes in media module

--- src/app/Raspi_App.cpp
+++ src/app/Raspi_App.cpp
@@ -61,15 +61,15 @@ namespace kinski
 
         esInitContext(m_context.get());
 
+        esCreateWindow(m_context.get(), name().c_str(),
+                       ES_WINDOW_RGB | ES_WINDOW_ALPHA | ES_WINDOW_DEPTH /*| ES_WINDOW_MULTISAMPLE*/);
+        
         // init gl::Context object
         auto platform_data = std::make_shared<gl::PlatformDataEGL>(m_context->eglDisplay,
                                                                    m_context->eglContext,
                                                                    m_context->eglSurface);
         gl::Context *ctx = new gl::Context(platform_data);
         (void)ctx;
-
-        esCreateWindow(m_context.get(), name().c_str(),
-                       ES_WINDOW_RGB | ES_WINDOW_ALPHA | ES_WINDOW_DEPTH /*| ES_WINDOW_MULTISAMPLE*/);
 
         // set graphical log stream
         Logger::get()->add_outstream(&m_outstream_gl);
