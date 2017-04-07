732b74979ab0fc9883a61c07ac687133770a73dd
crocdialer <crocdialer@googlemail.com>
2017-04-07 16:57:01 +0200

    time string formatting

--- src/projects/media_player/MediaPlayer.cpp
+++ src/projects/media_player/MediaPlayer.cpp
@@ -419,7 +419,7 @@ void MediaPlayer::reload_media()
 std::string MediaPlayer::secs_to_time_str(float the_secs) const
 {
     char buf[32];
-    sprintf(buf, "%d:%02d:%.1f", (int)the_secs / 3600, ((int)the_secs / 60) % 60, fmodf(the_secs, 60));
+    sprintf(buf, "%d:%02d:%04.1f", (int)the_secs / 3600, ((int)the_secs / 60) % 60, fmodf(the_secs, 60));
     return buf;
 }
 
