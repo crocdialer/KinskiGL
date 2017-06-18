d86141f21be9302d38e54d14aa932a1c035cd7ec
crocdialer <crocdialer@googlemail.com>
2017-06-18 19:05:01 +0200

    fixing compiler warnings

--- src/gl/QuadWarp.cpp
+++ src/gl/QuadWarp.cpp
@@ -69,7 +69,7 @@ namespace kinski{ namespace gl{
         
         inline bool is_corner(uint32_t the_index)
         {
-            int num_cp = (m_num_subdivisions.x + 1) * (m_num_subdivisions.y + 1);
+            uint32_t num_cp = (m_num_subdivisions.x + 1) * (m_num_subdivisions.y + 1);
             return  the_index == 0 ||
                     the_index == m_num_subdivisions.x ||
                     the_index == (num_cp - m_num_subdivisions.x - 1) ||
