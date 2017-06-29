d049f72ed6bb8b2d278d69170a84713227bfd201
crocdialer <crocdialer@googlemail.com>
2017-06-29 06:06:55 +0200

    minor change

--- src/gl/gl.cpp
+++ src/gl/gl.cpp
@@ -846,7 +846,7 @@ void draw_mesh(const MeshPtr &the_mesh, const ShaderPtr &overide_shader)
             {
                 uint32_t mat_index = the_mesh->entries()[i].material_index;
                 
-                if(mat_index >= 0 && mat_index < the_mesh->materials().size())
+                if(mat_index < the_mesh->materials().size())
                     mat_entries[mat_index].push_back(i);
             }
             
