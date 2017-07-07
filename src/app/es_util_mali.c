c4c5f7591f5ad897f393f13c84ba9f58a6e7b9b1
crocdialer <crocdialer@googlemail.com>
2017-07-07 17:50:24 +0200

    work on build files and display managment for mali

--- src/app/es_util_mali.c
+++ src/app/es_util_mali.c
@@ -4,7 +4,9 @@
 #include <stdarg.h>
 #include <sys/time.h>
 #include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
 #include <EGL/egl.h>
+#include <EGL/eglext.h>
 #include "esUtil.h"
 
 #include <xf86drm.h>
