d4b5662ce1eb77e2afe3fb9f8ad005da08cbe260
crocdialer <crocdialer@googlemail.com>
2017-07-15 17:54:50 +0200

    fixing ES3 bugs

--- src/app/es_util_mali.c
+++ src/app/es_util_mali.c
@@ -55,8 +55,6 @@ drmEventContext g_evctx =
 struct gbm_bo *g_bo = NULL;
 struct drm_fb *g_fb = NULL;
 
-static uint32_t g_previous_fb = 0;
-
 static uint32_t find_crtc_for_encoder(const drmModeRes *resources,
 				                      const drmModeEncoder *encoder)
 {
@@ -430,53 +428,41 @@ void ESUTIL_API esSwapBuffer(ESContext *esContext)
     next_bo = gbm_surface_lock_front_buffer(gbm.surface);
     g_fb = drm_fb_get_from_bo(next_bo);
 
-    // /*
-    //  * Here you could also update drm plane layers if you want
-    //  * hw composition
-    //  */
-    //
-    // int ret = drmModePageFlip(drm.fd, drm.crtc_id, g_fb->fb_id,
-    //                           DRM_MODE_PAGE_FLIP_EVENT, &waiting_for_flip);
-    // if(ret)
-    // {
-    //     printf("failed to queue page flip: %s\n", strerror(errno));
-    //     return;
-    // }
-    //
-    // while(waiting_for_flip)
-    // {
-    //     ret = select(drm.fd + 1, &g_fds, NULL, NULL, NULL);
-    //     if(ret < 0)
-    //     {
-    //         printf("select err: %s\n", strerror(errno));
-    //         return;
-    //     }
-    //     else if(ret == 0)
-    //     {
-    //         printf("select timeout!\n");
-    //         return;
-    //     }
-    //     else if (FD_ISSET(0, &g_fds))
-    //     {
-    //         printf("user interrupted!\n");
-    //         break;
-    //     }
-    //     drmHandleEvent(drm.fd, &g_evctx);
-    // }
-    //
-    // /* release last buffer to render on again: */
-    // gbm_surface_release_buffer(gbm.surface, g_bo);
-    // g_bo = next_bo;
+    /*
+     * Here you could also update drm plane layers if you want
+     * hw composition
+     */
 
-    uint32_t handle = gbm_bo_get_handle(g_bo).u32;
-	uint32_t pitch = gbm_bo_get_stride(g_bo);
-	uint32_t fb;
-	drmModeAddFB(gbm.dev, drm.mode->hdisplay, drm.mode->vdisplay, 24, 32, pitch, handle, &fb);
-    drmModeSetCrtc(gbm.dev, drm.crtc_id, fb, 0, 0, &drm.connector_id, 1, &drm.mode);
+    int ret = drmModePageFlip(drm.fd, drm.crtc_id, g_fb->fb_id,
+                              DRM_MODE_PAGE_FLIP_EVENT, &waiting_for_flip);
+    if(ret)
+    {
+        printf("failed to queue page flip: %s\n", strerror(errno));
+        return;
+    }
+
+    while(waiting_for_flip)
+    {
+        ret = select(drm.fd + 1, &g_fds, NULL, NULL, NULL);
+        if(ret < 0)
+        {
+            printf("select err: %s\n", strerror(errno));
+            return;
+        }
+        else if(ret == 0)
+        {
+            printf("select timeout!\n");
+            return;
+        }
+        else if (FD_ISSET(0, &g_fds))
+        {
+            printf("user interrupted!\n");
+            break;
+        }
+        drmHandleEvent(drm.fd, &g_evctx);
+    }
 
     /* release last buffer to render on again: */
-    drmModeRmFB(gbm.dev, &g_previous_fb);
     gbm_surface_release_buffer(gbm.surface, g_bo);
-    g_previous_fb = fb;
     g_bo = next_bo;
 }
