5eae5ce165c968f4138b6b5d48ffbde59f55cdf6
crocdialer <crocdialer@googlemail.com>
2017-09-12 12:03:01 +0200

    added playlist navigation via page up/down, fixed typo

--- src/projects/media_player/MediaPlayer.cpp
+++ src/projects/media_player/MediaPlayer.cpp
@@ -144,7 +144,8 @@ void MediaPlayer::draw()
         // time + playlist position
         auto str = secs_to_time_str(m_media->current_time()) + " / " +
             secs_to_time_str(m_media->duration());
-        str += m_playlist.empty() ? "" : format(" (%d / %d", m_current_playlist_index, m_playlist.size());
+        str += m_playlist.empty() ? "" : format(" (%d / %d)", m_current_playlist_index,
+                                                m_playlist.size());
         
         gl::draw_text_2D(str, fonts()[1], gl::COLOR_WHITE, gl::vec2(10, 40));
         
